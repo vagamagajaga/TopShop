8b96f673a74b0fb830d8d626ad8df4e2ef9005f1
Ваган <vagan@MacBook-Air-Sofa.local>
2025-05-17 21:43:38 +0700

    First commit

---
+++ TopShop/TopShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopShopApp.swift
+//  TopShop
+//
+//  Created by Ваган on 17.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct TopShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
