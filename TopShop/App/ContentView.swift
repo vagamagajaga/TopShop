e34f93dc50db2248e8554357e3c45b9124c37aa5
Ваган Галстян <vagan@MacBook-Air-Vagan.local>
2025-08-17 13:07:55 +0700

    Add BluredCard

---
+++ TopShop/App/ContentView.swift
@@ -0,0 +1,14 @@
+//
+//  ContentView.swift
+//  TopShop
+//
+//  Created by Ваган on 17.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Try it out!")
+    }
+}
---
+++ TopShop/SUIComponents/ProductCardBlured.swift
@@ -0,0 +1,102 @@
+//
+//  ProductCardBlured.swift
+//  TopShop
+//
+//  Created by Ваган Галстян on 16.08.2025.
+//
+
+import SwiftUI
+
+struct ProductCardBlured: View {
+    let image: ImageResource
+    let name: String
+    let price: String
+    let description: String
+    
+    var body: some View {
+        VStack {
+            Image(image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .overlay(alignment: .bottom) {
+                    VStack {
+                        Image(image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(height: 100, alignment: .bottom)
+                            .blur(radius: 4)
+                            .clipped()
+                    }
+                }
+        }
+        .overlay {
+            cardInfo()
+        }
+        .cornerRadius(20)
+        .frame(width: 400)
+        .padding(.horizontal, 12)
+        
+    }
+}
+
+
+private extension ProductCardBlured {
+    func buuton() -> some View {
+        Button(action: {
+            print("Buy button tapped")
+        }) {
+            Text("Add to Cart")
+                .font(.system(size: 16, weight: .semibold))
+                .foregroundColor(.black)
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 16)
+                .background(Color.white)
+                .cornerRadius(24)
+                .padding(EdgeInsets(top: 0, leading: 16, bottom: 12, trailing: 16))
+        }
+        .buttonStyle(PlainButtonStyle())
+    }
+    
+    func cardInfo() -> some View {
+        VStack(alignment: .leading) {
+            
+            Spacer()
+            
+            HStack {
+                Text(name)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+                    .frame(alignment: .leading)
+                
+                Spacer()
+                
+                Text(price)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+                    .frame(alignment: .leading)
+            }
+            .padding(.bottom, 12)
+            
+            Text(description)
+                .font(.body)
+                .foregroundStyle(.white)
+                .frame(alignment: .leading)
+            
+            buuton()
+        }
+        .padding(.horizontal, 16)
+        .padding(.bottom, 12)
+    }
+}
+
+#Preview {
+    ProductCardBlured(
+        image: .mango,
+        name: "Strawberry",
+        price: "9 $",
+        description: "Sweet, juicy and hand-picked."
+    )
+    
+}
